928d43da113fb783b2105c926c5c6744e9b25329
danielnam0801 <danielnam0801@gmail.com>
2022-07-12 13:44:18 +0900

    인게임ui대충완성

---
+++ JDJS_GJamProj/Assets/Scripts/Managers/GameUIManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIManager : MonoBehaviour
+{
+    [SerializeField] Text stageText;
+    [SerializeField] Button StatusBT;
+    [SerializeField] GameObject StatusPanel;
+    bool OntheStatus = false;
+    
+    void Start()
+    {
+        StatusPanel.SetActive(false);
+        //StartCoroutine("OnStatus");
+    }
+    private void Update()
+    {
+        
+        Vector2 mousePos = Input.mousePosition;
+        string message = mousePos.ToString();
+        Debug.Log(message);
+        if (mousePos.x <= 1890 && mousePos.x >= 1650 && mousePos.y >= 31.8 && mousePos.y <= 77.6)
+        {
+            StatusPanel.SetActive(true);
+        }
+        else
+        {
+            StatusPanel.SetActive(false);
+        }
+    }
+
+    //IEnumerator OnStatus()
+    //{
+    //    while (true)
+    //    {
+    //        if(OntheStatus == true)
+    //        {
+    //            OntheStatus = false;
+    //        }
+    //    }
+    //}
+
+}
